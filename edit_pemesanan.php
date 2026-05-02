4ed8a7829ef129cc779a38bddf1ac6676cba4715
Makfiyy <124250145@student.upnyk.ac.id>
2026-05-02 14:35:08 +0700

    tambah google font

--- edit_pemesanan.php
+++ edit_pemesanan.php
@@ -19,6 +19,8 @@ $d = mysqli_fetch_assoc($data);
 <head>
     <title>Edit Pemesanan</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
+
 </head>
 <body>
 
